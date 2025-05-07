2eda0a4846994488279d59a345044b2aabeee58c
Alex Cao <dreamwreckac@gmail.com>
2025-05-07 11:58:58 -0700

    2nd commit - added app.ge in server.js -

--- server.js
+++ server.js
@@ -16,4 +16,7 @@ const subscribersRouter = require('./routes/subscribers');
 app.use('/subscribers', subscribersRouter);
 
 const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('API is running!');
+  });
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
